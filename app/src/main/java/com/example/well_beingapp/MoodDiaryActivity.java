a049b8528aea6858f9cf99dc752a1bf9f1bc9b27
Arjun Gill <aarjunggill@gmail.com>
2020-07-20 11:50:06 +0100

    Changes made to page flow

---
+++ app/src/main/java/com/example/well_beingapp/MoodDiaryActivity.java
@@ -0,0 +1,14 @@
+package com.example.well_beingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoodDiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_diary);
+    }
+}
--- app/src/main/java/com/example/well_beingapp/SelfActivity.java
+++ app/src/main/java/com/example/well_beingapp/SelfActivity.java
@@ -1,13 +1,19 @@
 package com.example.well_beingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 
 public class SelfActivity extends AppCompatActivity {
+
+    Button logEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +22,18 @@ public class SelfActivity extends AppCompatActivity {
 
         Calendar calendar = Calendar.getInstance();
         String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
-
         TextView textViewDate = findViewById(R.id.textView10);
         textViewDate.setText(currentDate);
+
+        logEntry = findViewById(R.id.button2);
+        logEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intToMoodDiary = new Intent(SelfActivity.this, MoodDiaryActivity.class);
+                startActivity(intToMoodDiary);
+            }
+        });
+
     }
 
 }
